0d28d8283b3c5a0323e2d4cf689ecfa4b6b0ec70
Pedro Vanzella <pedro@pedrovanzella.com>
2011-08-04 17:35:05 -0300

    switching to carrierwave

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'mongoid'
 
 gem 'haml'
 
-gem 'paperclip'
+gem 'carrierwave'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,9 @@
 class Post
   include Mongoid::Document
-  include ::Paperclip
-  include ::Paperclip::Glue
 
   field :author
   field :author_email
   field :subject
   field :content
-
-  has_attached_file :picture,
-    path: ':attachment/:id/:style.:extension',
-    storage: :Filesystem
-
-  field :picture_file_name
-  field :picture_content_type
-  field :picture_file_size, type: String
-  field :picture_updated_at, type: DateTime
 
 end
