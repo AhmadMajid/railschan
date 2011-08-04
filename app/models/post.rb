5ebb0262427c1f6eb1e9e15447dc1551d071b5f7
Pedro Vanzella <pedro@pedrovanzella.com>
2011-08-04 16:35:05 -0300

    attached image

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post
   include Mongoid::Document
+  include Mongoid::Paperclip
+
   field :author
   field :author_email
   field :subject
   field :content
+
+  has_mongoid_attached_file :picture
 end
