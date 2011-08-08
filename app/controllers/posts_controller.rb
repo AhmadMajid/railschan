fb137c63f8ac5b63c7da7f0398f20e821c639d04
Pedro Vanzella <pedro@pedrovanzella.com>
2011-08-08 18:10:45 -0300

    reverting the IDs, since it breaks images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,8 @@ class PostsController < ApplicationController
   def create
     @board = Board.find(params[:board_id])
     @post = @board.posts.create!(params[:post])
-    @post.post_number = @board.posts.count + 1
-    @post.save!
+    #@post.post_number = @board.posts.count + 1
+    #@post.save!
     redirect_to @board, notice: 'Post successful'
   end
 
